4b17c8784b2421fcf91ddbd86d722ca67e411dbb
Brent Gardner <brent.gardner@zenplanner.com>
2014-10-02 23:07:40 -0600

    Working on lcd

--- src/main/java/com/zenplanner/App.java
+++ src/main/java/com/zenplanner/App.java
@@ -3,72 +3,80 @@ package com.zenplanner;
 import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.io.Resources;
-import sun.misc.IOUtils;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.*;
 
 public class App {
     private static final String filterCol = "partitionId";
     private static final List<String> smallTypes = Arrays.asList(new String[]{
-            "uniqueidentifier",
-            "bigint",
-            "date",
-            "datetime",
-            "datetime2",
-            "smalldatetime",
-            "tinyint",
-            "smallint",
-            "int",
-            "decimal",
-            "bit",
-            "money",
-            "smallmoney",
-            "char",
-            "float"
+            "uniqueidentifier", "bigint", "date", "datetime", "datetime2", "smalldatetime", "tinyint", "smallint",
+            "int", "decimal", "bit", "money", "smallmoney", "char", "float"
     });
     private static final List<String> bigTypes = Arrays.asList(new String[]{
-            "varchar",
-            "nvarchar",
-            "text"
+            "varchar", "nvarchar", "text"
     });
 
     public static void main(String[] args) throws Exception {
         String filterValue = args[0];
-        String sourceCon = args[1];
-        //String destCon = args[2];
+        String srcCon = args[1];
+        String dstCon = args[2];
 
         Class.forName("net.sourceforge.jtds.jdbc.Driver");
 
         // Get tables and columns
-        try (Connection scon = DriverManager.getConnection(sourceCon)) {
-            Map<String, Table> sourceTables = filterTables(getTables(scon));
-            for (Map.Entry<String, Table> entry : sourceTables.entrySet()) {
-                String tableName = entry.getKey();
-                Table table = entry.getValue();
-                String sql = writeHashedQuery(tableName, table, filterValue);
-                System.out.println("-----------------------------------------------\n");
-                System.out.println(sql);
+        try (Connection scon = DriverManager.getConnection(srcCon)) {
+            try (Connection dcon = DriverManager.getConnection(dstCon)) {
+                Map<String, Table> srcTables = filterTables(getTables(scon));
+                Map<String, Table> dstTables = filterTables(getTables(dcon));
+                for (Table srcTable : srcTables.values()) {
+                    if(!dstTables.containsKey(srcTable.getName())) {
+                        continue;
+                    }
+                    Table dstTable = dstTables.get(srcTable.getName());
+                    compTables(srcTable, dstTable);
+                }
             }
         }
     }
 
-    private static String writeHashedQuery(String tableName, Table table, String filterValue) {
+    private static void compTables(Table srcTable, Table dstTable) {
+        Table lcd = findLcd(srcTable, dstTable);
+        String sql = writeHashedQuery(lcd);
+    }
+
+    private static Table findLcd(Table srcTable, Table dstTable) {
+        Table table = new Table(srcTable.getName());
+        Set<String> colNames = new HashSet<>();
+        colNames.addAll(srcTable.keySet());
+        colNames.addAll(dstTable.keySet());
+        for(String colName : colNames) {
+            if(!srcTable.containsKey(colName) || !dstTable.containsKey(colName)) {
+                continue;
+            }
+            table.put(colName, srcTable.get(colName));
+        }
+        return table;
+    }
+
+    private static String writeHashedQuery(Table table) {
         List<String> colNames = new ArrayList<>();
-        for (Column col : table) {
+        for (Column col : table.values()) {
             colNames.add(getColSelect(col));
         }
         String selectClause = Joiner.on(",\n\t").join(colNames);
-        String sql = String.format("select\n\t%s from [%s]\nwhere [%s]='%s'",
-                selectClause, tableName, filterCol, filterValue);
+        String sql = String.format("select\n\t%s from [%s]\nwhere [%s]=?", selectClause, table.getName(), filterCol);
         return sql;
     }
 
     private static String getColSelect(Column col) {
-        if(smallTypes.contains(col.getDataType().toLowerCase())) {
+        if (smallTypes.contains(col.getDataType().toLowerCase())) {
             return "[" + col.getColumnName() + "]";
         }
-        if(bigTypes.contains(col.getDataType().toLowerCase())) {
+        if (bigTypes.contains(col.getDataType().toLowerCase())) {
             return String.format("HASHBYTES('md5', [%s]) as [%s]", col.getColumnName(), col.getColumnName());
         }
         throw new RuntimeException("Unknown type: " + col.getDataType());
@@ -82,7 +90,7 @@ public class App {
             boolean hasCol = false;
             String name = entry.getKey();
             Table table = entry.getValue();
-            for (Column col : table) {
+            for (Column col : table.values()) {
                 if (col.isPrimaryKey()) {
                     hasPk = true;
                 }
@@ -98,6 +106,7 @@ public class App {
     }
 
     private static Map<String, Table> getTables(Connection con) throws Exception {
+        // Collect the tables
         Map<String, Table> tables = new HashMap<>();
         try (Statement stmt = con.createStatement()) {
             String sql = Resources.toString(Resources.getResource("GetTables.sql"), Charsets.UTF_8);
@@ -105,15 +114,16 @@ public class App {
                 while (rs.next()) {
                     String tableName = rs.getString("table_name");
                     if (!tables.containsKey(tableName)) {
-                        tables.put(tableName, new Table());
+                        tables.put(tableName, new Table(tableName));
                     }
                     Table table = tables.get(tableName);
 
                     Column col = new Column();
-                    col.setColumnName(rs.getString("column_name"));
+                    String colName = rs.getString("column_name");
+                    col.setColumnName(colName);
                     col.setDataType(rs.getString("data_type"));
                     col.setPrimaryKey(rs.getBoolean("primary_key"));
-                    table.add(col);
+                    table.put(colName, col);
                 }
             }
         }
--- src/main/java/com/zenplanner/Table.java
+++ src/main/java/com/zenplanner/Table.java
@@ -1,6 +1,26 @@
 package com.zenplanner;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Table extends ArrayList<Column> {
+public class Table extends HashMap<String, Column> {
+    private final String name;
+
+    public Table(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Table)) {
+            return false;
+        }
+        return name.equalsIgnoreCase(((Table) obj).getName());
+    }
 }
