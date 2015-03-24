608651ad0538df9647604441b91a497093485d18
Gregg Petri <gregg@zenplanner.com>
2015-03-24 16:23:46 -0600

    Pass -PartitionId=<PartitionId> in command line

--- src/main/java/com/zenplanner/sql/App.java
+++ src/main/java/com/zenplanner/sql/App.java
@@ -10,28 +10,86 @@ import java.util.*;
 public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
+    private static final String conTemplate = "jdbc:jtds:sqlserver://%s:1433/%s;user=%s;password=%s";
+
+    private static final DbComparator comp = new DbComparator();
+
     public static void main(String[] args) throws Exception {
         Class.forName("net.sourceforge.jtds.jdbc.Driver");
 
-        if(args.length < 4) {
+        if (args.length == 0) {
             FormMain frm = new FormMain();
             frm.setVisible(true);
         } else {
-            Map<String, List<Object>> filters = new HashMap<>();
-            List<Object> vals = Arrays.asList(args[3].split(","));
-            filters.put(args[2], vals);
-            List<String> ignoreTables = new ArrayList<>(); // TODO: Allow command line input
-            sync(args[0], args[1], filters, ignoreTables);
-        }
-    }
+            Properties properties = comp.loadProps();
 
-    private static void sync(String srcCon, String dstCon, Map<String, List<Object>> filters, List<String> ignoreTables) throws Exception {
-        try (Connection scon = DriverManager.getConnection(srcCon)) {
-            try (Connection dcon = DriverManager.getConnection(dstCon)) {
-                DbComparator comp = new DbComparator();
-                comp.synchronize(scon, dcon, filters, ignoreTables, true);
+            String sourceServer = properties.getProperty("SourceServer");
+            String sourceDb = properties.getProperty("SourceDb");
+            String sourceUsername = properties.getProperty("SourceUsername");
+            String sourcePassword = properties.getProperty("SourcePassword");
+            String sourceCon = String.format(
+                    conTemplate,
+                    sourceServer,
+                    sourceDb,
+                    sourceUsername,
+                    sourcePassword);
+
+            String destServer = properties.getProperty("DestServer");
+            String destDb = properties.getProperty("DestDb");
+            String destUsername = properties.getProperty("DestUsername");
+            String destPassword = properties.getProperty("DestPassword");
+            String destCon = String.format(
+                    conTemplate,
+                    destServer,
+                    destDb,
+                    destUsername,
+                    destPassword);
+
+            Map<String, List<Object>> filters = new HashMap<>();
+            List<Object> filterColumns = new ArrayList<Object>();
+            // Filter column names must be lowercase to match table columns
+            if (args[0].startsWith("-PartitionId")) {
+                String[] partitionIdArg = args[0].split("=");
+                filters.put("partitionid", Arrays.asList(partitionIdArg[1]));
+            } else {
+                String filterValueList = properties.getProperty("FilterValue");
+                String[] filterValuesArray = filterValueList.split(",");
+                List<Object> filterValues = new ArrayList<Object>();
+                for (int i = 0; i < filterValuesArray.length; i++) {
+                    filterValues.add("[" + filterValuesArray[i] + "]");
+                }
+                filters.put("partitionid", filterValues);
+            }
+
+            String ignoreTablesList = properties.getProperty("IgnoreTables");
+            String[] ignoreTablesArray = ignoreTablesList.split(",");
+            List<String> ignoreTables = Arrays.asList(ignoreTablesArray);
+
+            boolean delete = (properties.getProperty("Delete")).equalsIgnoreCase("true");
+
+            try (Connection scon = DriverManager.getConnection(sourceCon)) {
+                System.out.println("Established source connection");
+                scon.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
+                try (Connection dcon = DriverManager.getConnection(destCon)) {
+                    System.out.println("Established destination connection");
+                    System.out.println("Filter Column");
+                    Set<String> filterColumnNames = filters.keySet();
+                    for (String filterColumnName : filterColumnNames) {
+                        System.out.println("- " + filterColumnName);
+                        List<Object> filterColumnValues = filters.get(filterColumnName);
+                        for (Object filterColumnValue : filterColumnValues) {
+                            System.out.println("-- " + filterColumnValue);
+                        }
+                    }
+                    System.out.println("Ignore Tables");
+                    for (String ignoreTable : ignoreTables) {
+                        System.out.println("- " + ignoreTable);
+                    }
+                    System.out.println("Delete=" + delete);
+                    comp.synchronize(scon, dcon, filters, ignoreTables, delete);
+                }
             }
         }
+        System.out.println("Done.");
     }
-
 }
