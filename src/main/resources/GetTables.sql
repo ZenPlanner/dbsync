2423fcfa6d8d6e8848300a6258bffe7d695a07ac
Gregg Petri <gregg@zenplanner.com>
2015-03-24 10:37:30 -0600

    Update Get Tables SQL to ignore identity columns and tables starting with underscore

--- src/main/resources/GetTables.sql
+++ src/main/resources/GetTables.sql
@@ -1,24 +1,23 @@
-SELECT
-  tab.name AS table_name,
-  col.name AS column_name,
-  typ.name AS DATA_TYPE,
-  CASE
-  WHEN pk.COLUMN_NAME IS NOT NULL THEN 1
-  ELSE 0
-  END      AS primary_key
-FROM sysobjects tab
-  INNER JOIN syscolumns col ON col.id = tab.id
-  INNER JOIN systypes typ ON typ.xtype = col.xtype AND typ.xusertype = col.xusertype
-  LEFT JOIN (
-              SELECT
-                tc.TABLE_NAME,
-                ccu.COLUMN_NAME
-              FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc
-                INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE ccu
-                  ON tc.CONSTRAINT_NAME = ccu.Constraint_name
-              WHERE tc.CONSTRAINT_TYPE = 'Primary Key'
-            ) pk
-    ON pk.TABLE_NAME = tab.name
-       AND pk.COLUMN_NAME = col.name
-WHERE tab.type = 'U'
-ORDER BY table_name, column_name;
+SELECT    tab.[name] AS "table_name",
+          col.[name] AS "column_name",
+          typ.[name] AS "data_type",
+         CASE
+            WHEN pk.[column_name] IS NOT NULL THEN 1
+           ELSE 0
+         END AS "primary_key"
+FROM     sys.tables tab
+  INNER JOIN sys.columns col ON col.[object_id] = tab.[object_id]
+  INNER JOIN sys.types typ ON typ.[system_type_id] = col.[system_type_id]
+    AND typ.[user_type_id] = col.[user_type_id]
+  LEFT OUTER JOIN (
+    SELECT  tc.[table_name],
+            ccu.[column_name]
+    FROM  information_schema.table_constraints tc
+      INNER JOIN information_schema.constraint_column_usage ccu ON tc.[constraint_name] = ccu.[constraint_name]
+    WHERE   tc.[constraint_type] = 'Primary Key'
+  ) pk ON pk.[table_name] = tab.[name] AND pk.[column_name] = col.[name]
+WHERE     tab.[type] = 'U'
+  AND     tab.[name] NOT LIKE '!_%' ESCAPE '!'
+  AND     col.[is_identity] = 0
+ORDER BY  [table_name],
+          [column_name]
